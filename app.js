9bb7101cc6e910543738e6839fc33455ce64b233
Siva Tejaswi <sivatejaswi27@gmail.com>
2024-07-23 12:45:06 +0530

    NETFLIX UI Clone

---
+++ app.js
@@ -0,0 +1,40 @@
+let accordian = document.getElementsByClassName("FAQ__title");
+
+for (let i = 0; i < accordian.length; i++) {
+  accordian[i].addEventListener("click", function () {
+    if (this.childNodes[1].classList.contains("fa-plus")) {
+      this.childNodes[1].classList.remove("fa-plus");
+      this.childNodes[1].classList.add("fa-times");
+    } else {
+      this.childNodes[1].classList.remove("fa-times");
+      this.childNodes[1].classList.add("fa-plus");
+    }
+
+    let content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+  document.querySelector('.hamburger-menu').addEventListener('click', function () {
+    document.querySelector('.navbar__nav__items').classList.toggle('active');
+});
+window.addEventListener('scroll', function () {
+    const scrollToTopBtn = document.querySelector('.scroll-to-top');
+    if (window.scrollY > 300) {
+        scrollToTopBtn.style.display = 'block';
+    } else {
+        scrollToTopBtn.style.display = 'none';
+    }
+});
+
+document.querySelector('.scroll-to-top').addEventListener('click', function () {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+
+
+}
